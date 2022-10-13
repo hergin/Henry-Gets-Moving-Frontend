e6b56e43fdf66488cc9327931fbeaa8ba8518864
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-13 16:00:42 -0400

    Initial Login styling

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,9 +1,24 @@
 import './Login.scss';
+import {Link} from "react-router-dom";
+import swings from '../../Assets/Swings.png';
 
 const Login = () => {
     return (
         <div className="login">
-
+            <p>Login</p>
+            <form>
+                <div className='email-input'>
+                    <label>Email</label>
+                    <input/>
+                </div>
+                <div className='buttons'>
+                    <button>Login</button>
+                    <Link to={'/register'}>Register</Link>
+                </div>
+            </form>
+            <div className='swings'>
+                <img src={swings}/>
+            </div>
         </div>
     )
 }
