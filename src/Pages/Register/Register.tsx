00fffd35c873850802991731ed1feef09d655117
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-13 16:21:34 -0400

    Initial Register styling

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,9 +1,25 @@
 import './Register.scss';
+import {Link} from "react-router-dom";
+import swings from "../../Assets/Swings.png";
 
 const Register = () => {
     return (
         <div className="register">
-
+            <p>Register</p>
+            <form>
+                <div className='email-input'>
+                    <label>Email</label>
+                    <input/>
+                    <label>Confirm Email</label>
+                    <input/>
+                </div>
+                <div className='buttons'>
+                    <button>Register</button>
+                </div>
+            </form>
+            <div className='swings'>
+                <img src={swings}/>
+            </div>
         </div>
     )
 }
