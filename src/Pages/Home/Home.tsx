f97ae465b23397d69062ac496f2b2037cdefb675
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-13 15:16:29 -0400

    Add initial desktop home page styling

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -12,12 +12,12 @@ const Home = () => {
             </div>
             <div className='home-content'>
                 <div className='of-the-day'>
-                    <p>Title</p>
+                    <p>Exercise of the Day</p>
                     <Link to={'/exercise'}><img src={exerciseImage}/></Link>
                     <Link to={'/exercise'}>Title</Link>
                 </div>
                 <div className='of-the-day'>
-                    <p>Title</p>
+                    <p>Recipe of the Day</p>
                     <Link to={'/recipes'}><img src={recipeImage}/></Link>
                     <Link to={'/recipes'}>Title</Link>
                 </div>
