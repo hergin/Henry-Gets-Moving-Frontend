1a1e4b59a871ef8a93d1b49a7459db883ed214ba
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-13 15:17:36 -0400

    Add alt to img tags

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -8,17 +8,17 @@ const Home = () => {
     return (
         <div className="home">
             <div className='book-container'>
-                <img src={homePhoto}/>
+                <img src={homePhoto} alt="Henry Gets Moving"/>
             </div>
             <div className='home-content'>
                 <div className='of-the-day'>
                     <p>Exercise of the Day</p>
-                    <Link to={'/exercise'}><img src={exerciseImage}/></Link>
+                    <Link to={'/exercise'}><img src={exerciseImage} alt="Exercise Photo"/></Link>
                     <Link to={'/exercise'}>Title</Link>
                 </div>
                 <div className='of-the-day'>
                     <p>Recipe of the Day</p>
-                    <Link to={'/recipes'}><img src={recipeImage}/></Link>
+                    <Link to={'/recipes'}><img src={recipeImage} alt="Recipe Photo"/></Link>
                     <Link to={'/recipes'}>Title</Link>
                 </div>
             </div>
