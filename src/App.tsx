d4db71a9ce928e6e74adc4deaa8ec08aa8e22036
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-13 11:44:14 -0400

    Add page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Header from '../src/Components/Header/Header';
+import About from "./Pages/About/About";
+import Calendar from "./Pages/Calendar/Calendar";
+import Exercise from "./Pages/Exercise/Exercise";
+import ExerciseLog from "./Pages/ExerciseLog/ExerciseLog";
+import Game from "./Pages/Game/Game";
+import Home from "./Pages/Home/Home";
+import IndividualRecipe from "./Pages/IndividualRecipe/IndividualRecipe";
+import LearnMore from "./Pages/LearnMore/LearnMore";
+import Login from "./Pages/Login/Login";
+import Recipe from "./Pages/Recipe/Recipe";
+import Register from "./Pages/Register/Register";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+        <Header/>
+         <Routes>
+             <Route path='/' element={<Home/>}/>
+             <Route path='/about' element={<About/>}/>
+             <Route path='/all-logs' element={<Calendar/>}/>
+             <Route path='/get-moving' element={<Exercise/>}/>
+             <Route path='/exercise-log' element={<ExerciseLog/>}/>
+             <Route path='/games' element={<Game/>}/>
+                {/*TODO Set this up so that the url is the recipe name*/}
+             <Route path='/individual-recipe' element={<IndividualRecipe/>}/>
+             <Route path='/learn-more' element={<LearnMore/>}/>
+             <Route path='/login' element={<Login/>}/>
+             <Route path='/recipes' element={<Recipe/>}/>
+             <Route path='/register' element={<Register/>}/>
+         </Routes>
+     </Router>
     </div>
   );
 }
