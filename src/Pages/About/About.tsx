c1e7037b0a3a30c3c4967f8f64459d112fdc0439
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-14 14:37:02 -0400

    About page styling

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,9 +1,44 @@
 import './About.scss';
+import teamPhoto from '../../Assets/HGMteam.jpg';
+import youtube from '../../Assets/YoutubeIcon.png';
+import facebook from '../../Assets/FacebookIcon.png';
 
 const About = () => {
     return (
         <div className="about">
-
+            <div className='team-photo'>
+                <img src={teamPhoto}/>
+            </div>
+            <div className='content'>
+                <h1>Henry Gets Moving Team</h1>
+                <div className='text'>
+                    <p>
+                        Henry the Hamster is a young boy who overcomes his obesity challenge with an active lifestyle
+                        and healthy eating. When we first meet Henry, he is overweight, having difficulty in school and
+                        at play. Like many children, Henry is teased for his weight. A discouraged Henry receives
+                        positive support from his friend, his physician, and his family to overcome his obesity and poor
+                        eating habits. Henry’s determination and his positive support network are the foundation of
+                        successful community wellness movements.
+                    </p>
+                    <p>
+                        Obesity is the foremost American public health problem. According to the Center for Disease
+                        Control and Prevention (CDC), 17% of children and 36% of adults are classified as obese, a
+                        condition that is strongly correlated with an increased risk of heart disease, stroke, and type
+                        II diabetes. The CDC estimates that obesity-related health care costs approach $150 billion.
+                        Increasingly sedentary lifestyles and diets with excessive quantities of fat, sugar, and
+                        processed foods have facilitated the tripling of childhood obesity and the doubling of adult
+                        obesity rates over the past 30 years.
+                    </p>
+                </div>
+                <div className='links'>
+                    <a href={'https://www.youtube.com/channel/UCf5XxC_7PcJx_t5vU9Es2pA'} target={"_blank"} className='yt'>
+                        <img src={youtube} alt='Youtube'/>
+                    </a>
+                    <a href={'https://m.facebook.com/profile.php?id=100063703680476&_rdr'} target={"_blank"}>
+                        <img src={facebook} alt='Facebook'/>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
