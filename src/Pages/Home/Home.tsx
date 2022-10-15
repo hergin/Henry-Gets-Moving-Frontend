2ccfbf9a18a5f7e8443680ab984225566da385a2
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-14 23:20:48 -0400

    Update background assets to not be background photo on home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,6 +3,10 @@ import homePhoto from '../../Assets/henry-gets-moving-home.jpg'
 import {Link} from "react-router-dom";
 import recipeImage from '../../Assets/recipeStock.jpg';
 import exerciseImage from '../../Assets/exerciseStock.jpg';
+import React from "react";
+// import footerImage from "../../Assets/HomeFooter.png";
+import footerImage from "../../Assets/grass.svg";
+import weather from '../../Assets/Weather.svg';
 
 const Home = () => {
     return (
@@ -11,17 +15,23 @@ const Home = () => {
                 <img src={homePhoto} alt="Henry Gets Moving"/>
             </div>
             <div className='home-content'>
-                <div className='of-the-day'>
-                    <p>Exercise of the Day</p>
-                    <Link to={'/exercise'}><img src={exerciseImage} alt="Exercise Photo"/></Link>
-                    <Link to={'/exercise'}>Title</Link>
+                <div className='weather-header'>
+                    <img src={weather} className='weather'/>
                 </div>
-                <div className='of-the-day'>
-                    <p>Recipe of the Day</p>
-                    <Link to={'/recipes'}><img src={recipeImage} alt="Recipe Photo"/></Link>
-                    <Link to={'/recipes'}>Title</Link>
-                </div>
+               <div className='otd-div'>
+                   <div className='of-the-day'>
+                       <p>Exercise of the Day</p>
+                       <Link to={'/exercise'}><img src={exerciseImage} alt="Exercise Photo"/></Link>
+                       <Link to={'/exercise'}>Title</Link>
+                   </div>
+                   <div className='of-the-day'>
+                       <p>Recipe of the Day</p>
+                       <Link to={'/recipes'}><img src={recipeImage} alt="Recipe Photo"/></Link>
+                       <Link to={'/recipes'}>Title</Link>
+                   </div>
+               </div>
             </div>
+            <img src={footerImage} className='footer'/>
         </div>
     )
 }
