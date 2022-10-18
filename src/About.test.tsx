6675ae9941350f9284d8c6310907057bdcf8e773
melody <melodylynnnh@gmail.com>
2022-10-18 13:25:33 -0400

    remove tests which technically aren't possible with jest

--- src/About.test.tsx
+++ src/About.test.tsx
@@ -12,19 +12,4 @@ describe('social media nav buttons',()=>{
             expect(screen.getByAltText('Facebook')).toBeInTheDocument();
         });
     });
-
-    describe('go to the right page',()=>{
-        test('youtube button',()=>{
-            render(<About/>);
-            fireEvent.click(screen.getByAltText('YouTube'));
-            expect(global.window.location.pathname).toContain('youtube.com');
-            expect(screen.getByText('Henry GetsMoving')).toBeInTheDocument();
-        });
-        test('facebook button',()=>{
-            render(<About/>);
-            fireEvent.click(screen.getByAltText('Facebook'));
-            expect(global.window.location.pathname).toContain('facebook.com');
-            expect(screen.getByText('Henry Gets Moving in Delaware County')).toBeInTheDocument();
-        });
-    })
 });
