0a23ee1d8d1ae4f2d0cfa81455b188769103b44f
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-19 10:25:40 -0400

    Add react helmet to game page

--- src/Pages/Game/Game.tsx
+++ src/Pages/Game/Game.tsx
@@ -1,9 +1,15 @@
 import './Game.scss';
+import {Helmet, HelmetProvider} from "react-helmet-async";
+import React from "react";
 
 const Game = () => {
     return (
         <div className="game">
-
+            <HelmetProvider>
+                <Helmet>
+                    <title>Game</title>
+                </Helmet>
+            </HelmetProvider>
         </div>
     )
 }
