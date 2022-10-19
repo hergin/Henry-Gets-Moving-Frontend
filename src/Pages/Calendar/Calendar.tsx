12b5cce617bc0b69eec7334119a7836b7f0e4038
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-19 10:25:05 -0400

    Add react helmet to calendar page

--- src/Pages/Calendar/Calendar.tsx
+++ src/Pages/Calendar/Calendar.tsx
@@ -1,9 +1,15 @@
 import './Calendar.scss'
+import {Helmet, HelmetProvider} from "react-helmet-async";
+import React from "react";
 
 const Calendar = () => {
     return (
         <div className="calendar">
-
+            <HelmetProvider>
+                <Helmet>
+                    <title>Calendar</title>
+                </Helmet>
+            </HelmetProvider>
         </div>
     )
 }
