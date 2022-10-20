45397e1fccbd85a4b0ebadc9c3d0c7e62f3062a2
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-20 11:16:19 -0400

    IndividualRecipe page desktop styling

--- src/Pages/IndividualRecipe/IndividualRecipe.tsx
+++ src/Pages/IndividualRecipe/IndividualRecipe.tsx
@@ -34,8 +34,7 @@ const IndividualRecipe = () => {
             <div className='recipe-text'>
                 <p>
                     Ingredients:
-                </p>
-                <p>
+                    <br/>
                     Steps: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
                     nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
