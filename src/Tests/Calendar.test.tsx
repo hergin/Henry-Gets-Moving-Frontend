189e6834d39dee354fd3695f6b5a02ad130f8799
melody <melodylynnnh@gmail.com>
2022-10-24 13:09:49 -0400

    add test month arrow buttons

--- src/Tests/Calendar.test.tsx
+++ src/Tests/Calendar.test.tsx
@@ -11,5 +11,15 @@ describe('date',()=>{
         render(<Calendar/>);
         fireEvent.click(screen.getByText('22'));
         expect(screen.getByText(new RegExp(`.*${new Date().getMonth()+1}/22/${new Date().toLocaleDateString().split('/')[2]}.*`))).toBeInTheDocument();
-    })
+    });
 });
+
+describe('month',()=>{
+    test('can be changed with arrows',()=>{
+        render(<Calendar/>);
+        const days = new Date().toLocaleDateString().split('/');
+        fireEvent.click(screen.getByText('›'));
+        fireEvent.click(screen.getByText(days[1]));
+        expect(screen.getByText(new RegExp(`.*${parseInt(days[0])+1}/${days[1]}/${days[2]}.*`))).toBeInTheDocument();
+    });
+})
