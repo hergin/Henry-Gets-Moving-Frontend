064cf35bf11bd8a582eb57084302bab8c4fb4c62
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-25 12:45:31 -0400

    Add weather desktop to weather component

--- src/Components/Weather.tsx
+++ src/Components/Weather.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import weather from "../Assets/Weather.svg";
+import weatherDesktop from '../Assets/WeatherDesktop.svg';
 import weatherMobile from "../Assets/WeatherMobile.svg";
 
 const Weather = () => {
     return (
         <div className='weather-div'>
             <img src={weather} className='weather' alt={"Weather"}/>
+            <img src={weatherDesktop} className='weather-desktop' alt={"Weather"}/>
             <img src={weatherMobile} className='weather-mobile' alt={"Weather"}/>
         </div>
     )
