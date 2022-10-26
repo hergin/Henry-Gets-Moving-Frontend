8517d2f9adabbc92d0211b5a7b75d1ffc07634dc
melody <melodylynnnh@gmail.com>
2022-10-26 13:17:47 -0400

    add game grid

--- src/Pages/Game/Game.tsx
+++ src/Pages/Game/Game.tsx
@@ -2,8 +2,30 @@ import './Game.scss';
 import {Helmet, HelmetProvider} from "react-helmet-async";
 import React from "react";
 import squirmMaze from '../../Assets/squirm_maze.png';
-
+import crossword from '../../Assets/crossword.png';
+type Game = {
+    [key: string]: any;
+    thumbnail: string;
+    name: string;
+}
 const Game = () => {
+    const games_list = [
+        {
+            thumbnail: crossword,
+            name: "Healthy Eating Crossword"
+        }
+    ];
+    const gamesLayout = (game: Game[]) => {
+        return game.map((game) => {
+                return (
+                    <div className='grid-content'>
+                        <img src={game.thumbnail} alt={game.name + "Thumbnail"}/>
+                        <p className='name'>{game.name}</p>
+                    </div>
+                )
+            }
+        )
+    }
     return (
         <div className="game">
             <HelmetProvider>
@@ -13,10 +35,9 @@ const Game = () => {
             </HelmetProvider>
             <div className="game-container">
                 <img src={squirmMaze} alt="Squirm's maze"/>
-                // TODO update to change with carousel game picker
             </div>
             <div className="game-picker">
-                
+                {gamesLayout(games_list)}
             </div>
         </div>
     )
