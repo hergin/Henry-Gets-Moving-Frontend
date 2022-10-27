b57903aadcde7a164dceb2c990eeb9710d18f7c5
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-27 12:12:18 -0400

    Basic admin panel set up

--- src/Pages/Admin/Admin.tsx
+++ src/Pages/Admin/Admin.tsx
@@ -1,9 +1,29 @@
 import './Admin.scss'
+import Weather from "../../Components/Weather";
+import Grass from "../../Components/Grass";
 
 const Admin = () => {
     return (
         <div className='admin'>
+            <Weather/>
+            <h1>Admin Panel</h1>
+            <div className='content'>
+                <div className="form-div">
+                    <form>
 
+                    </form>
+                    <form>
+
+                    </form>
+                </div>
+                <hr/>
+                <div className="form-div">
+                    <form>
+
+                    </form>
+                </div>
+            </div>
+            <Grass/>
         </div>
     )
 }
