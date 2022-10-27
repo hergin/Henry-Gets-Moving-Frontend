45e9b5dd271be5b9b62bc48084a9ea70337ea844
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-27 18:33:55 -0400

    Put forms in divs

--- src/Pages/Admin/Admin.tsx
+++ src/Pages/Admin/Admin.tsx
@@ -11,45 +11,49 @@ const Admin = () => {
             <h1>Admin Panel</h1>
             <div className='content'>
                 <div className="form-div">
-                    <form className='exercise-form'>
-                        <div className='add-edit'>
-                            <h2>Add Exercise</h2>
-                            <div className='edit-select'>
-                                <label>Edit Exercise</label>
+                    <div>
+                        <form className='exercise-form'>
+                            <div className='add-edit'>
+                                <h2>Add Exercise</h2>
+                                <div className='edit-select'>
+                                    <label>Edit Exercise</label>
+                                    <select></select>
+                                </div>
+                            </div>
+                            <div className='field'>
+                                <label>Name</label>
+                                <input/>
+                            </div>
+                            <div className='field'>
+                                <label>Video</label>
+                                <input/>
+                            </div>
+                            <div className='field'>
+                                <label>Category</label>
                                 <select></select>
                             </div>
-                        </div>
-                        <div className='field'>
-                            <label>Name</label>
-                            <input/>
-                        </div>
-                        <div className='field'>
-                            <label>Video</label>
-                            <input/>
-                        </div>
-                        <div className='field'>
-                            <label>Category</label>
-                            <select></select>
-                        </div>
-                        <div className='buttons'>
-                            <button className='delete'>Delete Exercise</button>
-                            <button className='save'>Save Exercise</button>
-                        </div>
-                    </form>
-                    <h2 className='otd'>Of the Day</h2>
-                    <form className='otd-form'>
-                        <div className='field'>
-                            <label>Recipe of the Day</label>
-                            <select className='otd-select'></select>
-                        </div>
-                        <div className='field'>
-                            <label>Exercise of the Day</label>
-                            <select className='otd-select'></select>
-                        </div>
-                        <div className='buttons'>
-                            <button className='save'>Save Changes</button>
-                        </div>
-                    </form>
+                            <div className='buttons'>
+                                <button className='delete'>Delete Exercise</button>
+                                <button className='save'>Save Exercise</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <h2 className='otd'>Of the Day</h2>
+                        <form className='otd-form'>
+                            <div className='field'>
+                                <label>Recipe of the Day</label>
+                                <select className='otd-select'></select>
+                            </div>
+                            <div className='field'>
+                                <label>Exercise of the Day</label>
+                                <select className='otd-select'></select>
+                            </div>
+                            <div className='buttons'>
+                                <button className='save'>Save Changes</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
                 <hr/>
                 <div className="form-div">
