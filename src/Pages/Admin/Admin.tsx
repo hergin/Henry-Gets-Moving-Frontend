d3df8c7c39e4d7eaa2945d07d98dc9e48c04ecde
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-10-27 14:04:44 -0400

    Add add recipe form

--- src/Pages/Admin/Admin.tsx
+++ src/Pages/Admin/Admin.tsx
@@ -34,6 +34,7 @@ const Admin = () => {
                             <button className='save'>Save Exercise</button>
                         </div>
                     </form>
+                    <h2>Of the Day</h2>
                     <form>
                         <div>
                             <label>Recipe of the Day</label>
@@ -51,7 +52,41 @@ const Admin = () => {
                 <hr/>
                 <div className="form-div">
                     <form>
-
+                        <div className='add-edit'>
+                            <h2>Add Recipe</h2>
+                            <div className='edit-select'>
+                                <label>Edit Recipe</label>
+                                <select></select>
+                            </div>
+                        </div>
+                        <div>
+                            <label>Name</label>
+                            <input/>
+                        </div>
+                        <div>
+                            <label>Thumbnail</label>
+                            <input type="file"/>
+                        </div>
+                        <div>
+                            <label>Category</label>
+                            <select></select>
+                        </div>
+                        <div>
+                            <label>Cook Time</label>
+                            <input/>
+                        </div>
+                        <div>
+                            <label>Ingredients</label>
+                            <textarea/>
+                        </div>
+                        <div>
+                            <label>Recipe Steps</label>
+                            <textarea/>
+                        </div>
+                        <div className='buttons'>
+                            <button className='delete'>Delete Exercise</button>
+                            <button className='save'>Save Exercise</button>
+                        </div>
                     </form>
                 </div>
             </div>
