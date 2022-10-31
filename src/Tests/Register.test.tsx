2c7b64768703413fa51c6bff6b335f2c8d0a6005
melody <melodylynnnh@gmail.com>
2022-10-31 12:25:36 -0400

    add test email box exists

---
+++ src/Tests/Register.test.tsx
@@ -0,0 +1,6 @@
+import {render,screen,fireEvent} from "@testing-library/react";
+import {BrowserRouter as Router} from 'react-router-dom';
+import App from '../App';
+import Register from '../Pages/Register/Register';
+
+describe()
