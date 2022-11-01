34ee0027ca7ce5aa001da364d7f36b07462702f1
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-11-01 12:02:57 -0400

    make functional back arrow

---
+++ src/Components/BackArrow/BackArrow.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import back from '../../Assets/BackArrow.svg';
+
+interface BackArrowProps {
+    route: string;
+}
+
+export default function BackArrow({route}: BackArrowProps) {
+    return (
+        <div className="back-arrow">
+            <Link to={route}>
+                <img src={back} alt="Back"/>
+                <p>Back</p>
+            </Link>
+        </div>
+    );
+}
