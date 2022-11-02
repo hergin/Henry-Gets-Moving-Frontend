bbe7e19f180aabd1545adbf6e02dd7caac62d3e4
melody <melodylynnnh@gmail.com>
2022-11-02 12:39:10 -0400

    add tests: video player, category selector, etc

--- src/Tests/Exercise.test.tsx
+++ src/Tests/Exercise.test.tsx
@@ -1,4 +1,4 @@
-import {render,screen} from '@testing-library/react';
+import {fireEvent, render,screen} from '@testing-library/react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from '../App';
 import Exercise from '../Pages/Exercise/Exercise';
@@ -20,6 +20,47 @@ describe('trophy',()=>{
         render(<Router><Exercise/></Router>);
         expect(screen.getByAltText('Trophy')).toBeInTheDocument();
     });
+    test('text exists',()=>{
+        render(<Router><Exercise/></Router>);
+        expect(screen.getByText('You Have Logged')).toBeInTheDocument();
+    });
     test.todo('image updates with exercise logged');
     test.todo('text updates with exercise logged');
 });
+
+test('log exercise button takes to correct url',()=>{
+    render(<Router><App/></Router>);
+    fireEvent.click(screen.getByText("Get Moving"));
+    fireEvent.click(screen.getByText('Log Exercise'));
+    expect(global.window.location.pathname).toContain('/exercise-log');
+});
+
+test('all logs button takes to calendar',()=>{
+    render(<Router><App/></Router>);
+    fireEvent.click(screen.getByText('Get Moving'));
+    fireEvent.click(screen.getByText(/(All Logs)$/));
+    expect(global.window.location.pathname).toContain('/all-logs');
+});
+
+test('category selector exists in document',()=>{
+    render(<Router><Exercise/></Router>);
+    expect(screen.getByText('Category Selection')).toBeInTheDocument();
+});
+
+describe('video player',()=>{
+    test('opens on click',()=>{
+        render(<Router><Exercise/></Router>);
+        // TODO: update when exercises added
+        fireEvent.click(screen.getAllByAltText('Some nameThumbnail')[0]);
+        expect(screen.getByAltText('Exit')).toBeInTheDocument();
+    });
+    test('exit button works',()=>{
+        render(<Router><Exercise/></Router>);
+        // TODO: update when exercises added
+        fireEvent.click(screen.getAllByAltText('Some nameThumbnail')[0]);
+        fireEvent.click(screen.getByAltText('Exit'));
+        expect(screen.queryByAltText('Exit')).not.toBeInTheDocument();
+    });
+});
+
+test.todo('see more button expands');
