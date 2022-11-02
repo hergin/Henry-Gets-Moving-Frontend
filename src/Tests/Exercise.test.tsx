85788e99b92a0e7d6faa8e09d1166987ddfb4049
melody <melodylynnnh@gmail.com>
2022-11-02 12:22:57 -0400

    add tests and todos

--- src/Tests/Exercise.test.tsx
+++ src/Tests/Exercise.test.tsx
@@ -1,3 +1,25 @@
 import {render,screen} from '@testing-library/react';
 import {BrowserRouter as Router} from 'react-router-dom';
+import App from '../App';
+import Exercise from '../Pages/Exercise/Exercise';
 
+describe('of the day',()=>{
+    test('thumbnail exists',()=>{
+        render(<Router><Exercise/></Router>)
+        expect(screen.getByAltText('OTD Thumbnail')).toBeInTheDocument();
+    });
+    test('name exists',()=>{
+        // TODO: update when OTD implemented
+        render(<Router><Exercise/></Router>);
+        expect(screen.getByText('Exercise Name')).toBeInTheDocument();
+    });
+});
+
+describe('trophy',()=>{
+    test('image exists',()=>{
+        render(<Router><Exercise/></Router>);
+        expect(screen.getByAltText('Trophy')).toBeInTheDocument();
+    });
+    test.todo('image updates with exercise logged');
+    test.todo('text updates with exercise logged');
+});
