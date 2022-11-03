dfeab1654cbf2e0a6610152adac4876673b22c0c
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-11-03 11:33:48 -0400

    check if emails match and disable if not

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,12 +1,23 @@
 import './Register.scss';
 import swings from "../../Assets/Swings.png";
-import React from "react";
+import React, {useState} from "react";
 import {Helmet, HelmetProvider} from "react-helmet-async";
 import Weather from "../../Components/Weather";
 import Grass from "../../Components/Grass";
 import BackArrow from "../../Components/BackArrow/BackArrow";
 
 const Register = () => {
+    const [email, setEmail] = useState("");
+    const [confirmEmail, setConfirmEmail] = useState("");
+
+    function checkEmailsMatch() {
+        return email === confirmEmail && email.length > 0 && confirmEmail.length > 0;
+    }
+
+    function handleSubmit(event: { preventDefault: () => void; }) {
+        event.preventDefault();
+    }
+
     return (
         <div className="register">
             <HelmetProvider>
@@ -17,15 +28,15 @@ const Register = () => {
             <Weather/>
             <BackArrow route="/login"/>
             <h1>Register</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='email-input'>
                     <label htmlFor='email-box'>Email</label>
-                    <input id='email-box'/>
+                    <input id='email-box' onChange={(e) => setEmail(e.target.value)}/>
                     <label htmlFor='confirm-email-box'>Confirm Email</label>
-                    <input id='confirm-email-box'/>
+                    <input id='confirm-email-box'  onChange={(e) => setConfirmEmail(e.target.value)}/>
                 </div>
                 <div className='buttons'>
-                    <button className='red-button'>Register</button>
+                    <button className='red-button' type="submit" disabled={!checkEmailsMatch()}>Register</button>
                 </div>
             </form>
             <div className='swings'>
