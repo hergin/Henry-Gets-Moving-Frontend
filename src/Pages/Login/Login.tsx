f797e8140e3ff76e730f0100a8fe5aa6506b5dd8
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-11-03 11:24:07 -0400

    add validate form/disable login button if nothing in input field

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,12 +1,22 @@
 import './Login.scss';
 import {Link} from "react-router-dom";
 import swings from '../../Assets/Swings.png';
-import React from "react";
+import React, {useState} from "react";
 import {Helmet, HelmetProvider} from "react-helmet-async";
 import Weather from "../../Components/Weather";
 import Grass from "../../Components/Grass";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+
+    function validateForm() {
+        return email.length > 0;
+    }
+
+    function handleSubmit(event: { preventDefault: () => void; }) {
+        event.preventDefault();
+    }
+
     return (
         <div className="login">
             <HelmetProvider>
@@ -16,20 +26,20 @@ const Login = () => {
             </HelmetProvider>
             <Weather/>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='email-input'>
                     <label>Email</label>
-                    <input/>
+                    <input onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className='buttons'>
-                    <button className='red-button'>Login</button>
+                    <button className='red-button' type="submit" disabled={!validateForm()}>Login</button>
                     <Link to={'/register'} className='register-button red-button'>Register</Link>
                 </div>
             </form>
             <div className='swings'>
                 <img src={swings} alt={"Henry and Jasmine on Swings"}/>
             </div>
-          <Grass/>
+            <Grass/>
         </div>
     )
 }
