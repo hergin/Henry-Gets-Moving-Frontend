7f294f34d7cbfb7e9fd910e2075cad27e8c34010
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-11-03 12:39:45 -0400

    add form functionality

--- src/Pages/ExerciseLog/ExerciseLog.tsx
+++ src/Pages/ExerciseLog/ExerciseLog.tsx
@@ -1,6 +1,6 @@
 import './ExerciseLog.scss';
 import swings from "../../Assets/Swings.png";
-import React from "react";
+import React, {useState} from "react";
 import light from '../../Assets/lightIntensityFace.svg';
 import medium from '../../Assets/mediumIntensityFace.svg';
 import hard from '../../Assets/intenseIntensityFace.svg';
@@ -8,8 +8,47 @@ import {Helmet, HelmetProvider} from "react-helmet-async";
 import Weather from "../../Components/Weather";
 import Grass from "../../Components/Grass";
 import BackArrow from "../../Components/BackArrow/BackArrow";
+import {useNavigate} from "react-router-dom";
 
 const ExerciseLog = () => {
+    const [child, setChild] = useState("");
+    const [exercise, setExercise] = useState("");
+    const [intensity, setIntensity] = useState("");
+    const [duration, setDuration] = useState("");
+    const navigate = useNavigate();
+
+
+    function checkAllFormsFilled() {
+        return child.length > 0 && exercise.length > 0 && intensity.length > 0 && duration.length > 0;
+    }
+
+    function handleSubmit(e: { preventDefault: () => void; }) {
+        e.preventDefault();
+        // return fetch(`${API_URL}/users`, {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify({
+        //         email: email
+        //     })
+        // })
+        //     .then(response => {
+        //         if(response.ok) {
+        //             return response.json();
+        //         }
+        //         throw new Error("Invalid email");
+        //     })
+        //     .then(response => {
+        //         // sessionStorage.setItem("session_key", response.token);
+        //         navigate("/login");
+        //     })
+        //     .catch(err => {
+        //         // sessionStorage.clear();
+        //         window.alert(err);
+        //     })
+    }
+
     return (
         <div className="exercise-log">
             <HelmetProvider>
@@ -20,28 +59,28 @@ const ExerciseLog = () => {
             <Weather/>
             <BackArrow route="/get-moving"/>
             <h1>Exercise Log</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='log-input'>
                     <div className='label-input'>
                         <label>Child's Name</label>
-                        <input/>
+                        <input value={child} onChange={(e) => setChild(e.target.value)}/>
                     </div>
                     <div className='label-input'>
                         <label>Exercise Type</label>
-                        <input/>
+                        <input value={exercise} onChange={(e) => setExercise(e.target.value)}/>
                     </div>
                     <div className='intensity label-input'>
                         <label>Intensity</label>
                         <div className="icons">
-                            <div className='intensity-icon'>
-                                <img src={light} alt={"Light Intensity"}/>
+                            <div className='intensity-icon' onClick={(e => setIntensity("Light"))}>
+                                <img src={light} alt={"Light Intensity"} />
                                 <p>Light</p>
                             </div>
-                            <div className='intensity-icon'>
+                            <div className='intensity-icon' onClick={(e => setIntensity("Moderate"))}>
                                 <img src={medium} alt={"Moderate Intensity"}/>
                                 <p>Moderate</p>
                             </div>
-                            <div className='intensity-icon'>
+                            <div className='intensity-icon' onClick={(e => setIntensity("Vigorous"))}>
                                 <img src={hard} alt={"Vigorous Intensity"}/>
                                 <p>Vigorous</p>
                             </div>
@@ -49,11 +88,11 @@ const ExerciseLog = () => {
                     </div>
                     <div className='label-input'>
                         <label>Duration</label>
-                        <input placeholder="# of Minutes"/>
+                        <input placeholder="# of Minutes" value={duration} onChange={(e) => setDuration(e.target.value)}/>
                     </div>
                 </div>
                 <div className='buttons'>
-                    <button className='red-button'>Log Exercise</button>
+                    <button className='red-button' type="submit" disabled={!checkAllFormsFilled()}>Log Exercise</button>
                 </div>
             </form>
             <div className='swings'>
