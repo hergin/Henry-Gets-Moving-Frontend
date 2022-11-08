c5ef7654ffb733e7aff63a8576d02917c882d999
Mars <ngwilliams@bsu.edu>
2022-11-08 15:53:11 -0500

    Add Exercise and ExerciseCategory to DataTypes.ts

--- src/Structs/DataTypes.ts
+++ src/Structs/DataTypes.ts
@@ -10,3 +10,16 @@ export type RecipeCategory = {
     id: number,
     name: string
 }
+
+export type Exercise = {
+    id: number,
+    name: string,
+    videoLink: string,
+    isFeatured: boolean,
+    category_id: number,
+    exerciseCategory: ExerciseCategory
+}
+export type ExerciseCategory = {
+    id: number,
+    name: string
+}
