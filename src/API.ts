c62408a08bfef4f3cd3a8e4a71adab30b6b73113
Mars <ngwilliams@bsu.edu>
2022-11-08 16:08:29 -0500

    Create API.ts and add getRecipes function

---
+++ src/API.ts
@@ -0,0 +1,18 @@
+import {Recipe, Exercise, RecipeCategory, ExerciseCategory} from "./Structs/DataTypes";
+
+const getRecipes = async (): Promise<Recipe[]> => {
+    return await fetch('http://127.0.0.1:3333/recipes')
+        .then((response) => {
+        if (response.ok) return response.json();
+        return {
+            errorCode: response.status,
+            error: response.statusText,
+        }
+    }).then((response) => {
+        return response.map((recipe: any) => {
+            return {
+                ...recipe
+            } as Recipe
+        });
+    });
+}
