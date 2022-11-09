dfefcddbae8187361fdfa22822a2071a3217fc32
Mars <ngwilliams@bsu.edu>
2022-11-09 13:30:42 -0500

    Add link to Individual Recipe and hook up backend

--- src/Pages/IndividualRecipe/IndividualRecipe.tsx
+++ src/Pages/IndividualRecipe/IndividualRecipe.tsx
@@ -1,39 +1,48 @@
 import './IndividualRecipe.scss';
-import React from "react";
+import React, {useEffect, useState} from "react";
 import recipeStock from '../../Assets/recipeStock.jpg';
 import {Helmet, HelmetProvider} from "react-helmet-async";
 import Weather from "../../Components/Weather";
 import Grass from "../../Components/Grass";
 import BackArrow from "../../Components/BackArrow/BackArrow";
+import {Recipe} from "../../Structs/DataTypes";
+import {useParams} from "react-router-dom";
+
 
 const IndividualRecipe = () => {
+    const [recipe, setRecipe] = useState<Recipe>()
+    const id = useParams();
+    useEffect( () => {
+        const getRecipe = async() => {
+            const recipeFromServer = await fetch(`http://127.0.0.1:3333/recipes/${id.id}`)
+            setRecipe(await recipeFromServer.json())
+        }
+        getRecipe()
+    }, [])
     return (
         <div className="individual-recipe">
             <HelmetProvider>
                 <Helmet>
-                    <title>RecipePage Name</title>
+                    <title>{recipe?.name}</title>
                 </Helmet>
             </HelmetProvider>
             <Weather/>
             <BackArrow route="/recipes"/>
             <div className='otd-div'>
                 <div className='otd-image'>
-                    <img src={recipeStock} alt='RecipePage Name'/>
+                    <img src={recipe?.thumbnail} alt='RecipePage Name'/>
                 </div>
                 <div className='otd-text'>
-                    <h2>RecipePage of the Day</h2>
-                    <p>Cook Time</p>
+                    <h2>{recipe?.name}</h2>
+                    <p>{recipe?.cook_time}</p>
                 </div>
             </div>
             <div className='recipe-text'>
                 <p>
-                    Ingredients:
-                    <br/>
-                    Steps: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
+                    Ingredients: {recipe?.ingredients}
+                </p>
+                <p>
+                    Steps: {recipe?.recipe_steps}
                 </p>
             </div>
             <Grass/>
