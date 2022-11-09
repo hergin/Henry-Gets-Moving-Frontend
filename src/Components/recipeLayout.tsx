18b59666f2aad309431ea8c5a7fd78371635674a
Mars <ngwilliams@bsu.edu>
2022-11-08 22:44:36 -0500

    Create recipeLayout component

---
+++ src/Components/recipeLayout.tsx
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom";
+import React from "react";
+import {Recipe} from "../Structs/DataTypes";
+import individualRecipe from "../Pages/IndividualRecipe/IndividualRecipe";
+
+const recipeLayout = (individualRecipe: Recipe[]) => {
+    return individualRecipe.map((recipe) => {
+            return (
+                <div className='grid-content'>
+                    <Link to={'/individual-recipe'}><img src={recipe.thumbnail} alt={recipe.name + "Thumbnail"}/></Link>
+                    <p className='name'>{recipe.name}</p>
+                    <p className='category'>{recipe.recipeCategory?.name}</p>
+                </div>
+            )
+        }
+    )
+}
+
+export default recipeLayout
