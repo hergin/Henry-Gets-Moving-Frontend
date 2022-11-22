ccfcfc170b2f56867289e3d18ef884dbd3eb084c
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-11-22 13:18:40 -0500

    check if menu is open and toggle

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,18 +1,25 @@
 import './Nav.scss'
 import Hamburger from "../Hamburger/Hamburger";
+import {useState} from "react";
 
 export default function Nav() {
+    const [open, setOpen] = useState(false);
+
+    const toggleMenu =()=>{
+        setOpen(!open)
+    }
+
     return (
         <div className='menu-nav'>
             <div className='navigation'>
-                <ul>
+                <ul className={open ? 'open' : 'closed'}>
                     <li>Get Moving</li>
                     <li>Recipes</li>
                     <li>Learn More</li>
                     <li>About</li>
                     <li>Login</li>
                 </ul>
-                <div className='hamburger'>
+                <div className='hamburger' onClick={toggleMenu}>
                     <Hamburger/>
                 </div>
             </div>
