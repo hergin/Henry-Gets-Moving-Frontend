73fa8f3fb79b8d456c9c3ece36c271389590bfb7
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-11-22 13:56:09 -0500

    add squirm to hamburger menu if logged in

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -2,12 +2,22 @@ import './Nav.scss'
 import Hamburger from "../Hamburger/Hamburger";
 import {useState} from "react";
 import {Link} from "react-router-dom";
+import API from "../../API";
+import squirm from "../../Assets/worm.png";
 
 export default function Nav() {
     const [open, setOpen] = useState(false);
 
     const toggleMenu = () => {
         setOpen(!open)
+    }
+
+    function headerLogIn() {
+        if (API.isLoggedIn()) {
+            return <div className='squirm'><img src={squirm} alt=""/></div>
+        } else {
+            return <Link to={'/login'} className='login-button'>Login</Link>
+        }
     }
 
     return (
@@ -18,7 +28,7 @@ export default function Nav() {
                     <li><Link to={'/recipes'}>Recipes</Link></li>
                     <li><Link to={'/learn-more'}>Learn More</Link></li>
                     <li><Link to={'/about'}>About</Link></li>
-                    <li><Link to={'/login'}>Login</Link></li>
+                    <li>{headerLogIn()}</li>
                 </ul>
                 <div className='hamburger' onClick={toggleMenu}>
                     <Hamburger isOpen={open}/>
