1ca1161dda9eb4fbcc88555be55e6cc0ced7649b
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-11-22 11:41:36 -0500

    add nav component file

---
+++ src/Components/Nav/Nav.tsx
@@ -0,0 +1,11 @@
+import './Nav.scss'
+
+export default function Nav() {
+    return (
+        <div className='menu-nav'>
+            <div className='navigation'>
+
+            </div>
+        </div>
+    )
+}
