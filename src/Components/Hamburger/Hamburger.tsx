f99dcf51e581a8f1a0f60d2027beb9615d8279cd
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-11-22 12:31:41 -0500

    add hamburger lines

--- src/Components/Hamburger/Hamburger.tsx
+++ src/Components/Hamburger/Hamburger.tsx
@@ -3,7 +3,11 @@ import './Hamburger.scss'
 export default function Hamburger() {
     return (
         <div className='menu-hamburger'>
-
+            <div className='hamburger'>
+                <div className='burger burger1'/>
+                <div className='burger burger2'/>
+                <div className='burger burger3'/>
+            </div>
         </div>
     )
 }
