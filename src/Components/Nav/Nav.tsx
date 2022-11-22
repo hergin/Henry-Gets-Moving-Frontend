e26920978716647ffc6fde2e2c93ee68f44b815b
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-11-22 11:47:25 -0500

    add li tags

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -4,7 +4,13 @@ export default function Nav() {
     return (
         <div className='menu-nav'>
             <div className='navigation'>
-
+                <ul>
+                    <li>Get Moving</li>
+                    <li>Recipes</li>
+                    <li>Learn More</li>
+                    <li>About</li>
+                    <li>Login</li>
+                </ul>
             </div>
         </div>
     )
