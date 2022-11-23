0352d9f0f04d72fcf05effa5fcf639da9b45548c
melody <melodylynnnh@gmail.com>
2022-11-23 18:08:52 -0500

    add `createdAt` to `ExerciseLog` datatype

--- src/Structs/DataTypes.ts
+++ src/Structs/DataTypes.ts
@@ -46,5 +46,6 @@ export type ExerciseLog = {
     intensity: string,
     duration: string,
     family_member_name: string,
-    FamilyMember?: FamilyMember
+    FamilyMember?: FamilyMember,
+    createdAt: Date
 }
