2fcfa7c53bee925f1790b1a6da652e005243ce51
melody <melodylynnnh@gmail.com>
2022-11-23 12:42:42 -0500

    add test api

---
+++ src/Tests/TestAPI.ts
@@ -0,0 +1,149 @@
+import { FamilyMember, User } from "../Structs/DataTypes";
+
+let users = [
+    {
+        id: 1,
+        email: "whatever@bsu.edu",
+        remember_me_token: "test",
+        createdAt: new Date(2022, 10, 25, 0,0,0,0)
+    },
+    {
+        id: 2,
+        email: "other.email@bsu.edu",
+        remember_me_token: "whatever",
+        createdAt: new Date(2022, 10, 30, 0,0,0,0)
+    }
+];
+
+let family_members = [
+    {
+        id: 1,
+        name: "Bill",
+        user_id: 1
+    },
+    {
+        id: 2,
+        name: "John",
+        user_id: 1
+    },
+    {
+        id: 3,
+        name: "Kelly",
+        user_id: 2
+    },
+    {
+        id: 4,
+        name: "Sandy",
+        user_id: 2
+    }
+];
+let exercise_logs = [
+    {
+        id: 1,
+        type: "Yoga",
+        intensity: "Vigorous",
+        duration: 20,
+        family_member_id: 1,
+        createdAt: new Date(2022, 10, 30, 0,0,0,0)
+    },
+    {
+        id: 2,
+        type: "Running",
+        intensity: "Moderate",
+        duration: 10,
+        family_member_id: 1,
+        createdAt: new Date(2022, 10, 30, 0,0,0,0)
+    },
+    {
+        id: 3,
+        type: "Burpees",
+        intensity: "Light",
+        duration: 40,
+        family_member_id: 2,
+        createdAt: new Date(2022, 10, 30, 0,0,0,0)
+    }
+];
+
+const recipes = [
+    {
+        id: 1,
+        name: "Banana Bread",
+        thumbnail: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.tasteofhome.com%2Fwp-content%2Fuploads%2F2018%2F01%2FEasy-Banana-Bread_exps7423_TH.CW1973175D03_26_1b_RMS-1.jpg",
+        cook_time: "20 years",
+        ingredients: "Banana, Bread",
+        recipe_steps: "Step 1: cook\nStep 2: eat",
+        category_id: 1,
+        is_featured: true
+    },
+    {
+        id: 2,
+        name: "Bread Bread",
+        thumbnail: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.tasteofhome.com%2Fwp-content%2Fuploads%2F2018%2F01%2FEasy-Banana-Bread_exps7423_TH.CW1973175D03_26_1b_RMS-1.jpg",
+        cook_time: "20 millennia",
+        ingredients: "Bread, Bread",
+        recipe_steps: "Step 1: cook\nStep 2: DON'T eat",
+        category_id: 1,
+        is_featured: false
+    },
+    {
+        id: 3,
+        name: "Super Bread",
+        thumbnail: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.tasteofhome.com%2Fwp-content%2Fuploads%2F2018%2F01%2FEasy-Banana-Bread_exps7423_TH.CW1973175D03_26_1b_RMS-1.jpg",
+        cook_time: "20 eternities",
+        ingredients: "Superpowers, Bread",
+        recipe_steps: "Step 1: create\nStep 2: LIVE",
+        category_id: 2,
+        is_featured: false
+    },
+    {
+        id: 4,
+        name: "Not Bread",
+        thumbnail: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.tasteofhome.com%2Fwp-content%2Fuploads%2F2018%2F01%2FEasy-Banana-Bread_exps7423_TH.CW1973175D03_26_1b_RMS-1.jpg",
+        cook_time: "20 picoseconds",
+        ingredients: "Air",
+        recipe_steps: "Breathe",
+        category_id: 2,
+        is_featured: false
+    }
+];
+
+const recipe_categories = [
+    {
+        id: 1,
+        name: "Various Breads"
+    },
+    {
+        id: 2,
+        name: "Various Maybe Breads"
+    }
+];
+
+export function getExerciseLogs(member: FamilyMember) {
+    let logs = [];
+    for (let i = 0; i < 3; i++) {
+        if (exercise_logs[i].family_member_id == member.id)
+            logs.push(exercise_logs[i]);
+    }
+    return Promise.resolve(logs);
+}
+
+export function getFamilyMembers(user: User) {
+    let members = [];
+    for (let i = 0; i < 4; i++) {
+        if (family_members[i].user_id == user.id)
+            members.push(family_members[i]);
+    }
+    return Promise.resolve(members);
+}
+
+export function getRecipes() {
+    return Promise.resolve(recipes);
+}
+
+const TestAPI = {
+    getExerciseLogs,
+    getFamilyMembers,
+    getRecipes
+};
+
+export default TestAPI;
