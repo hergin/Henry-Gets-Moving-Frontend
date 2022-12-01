51e8b4eed184dee4920ad587cef32f6ad7d6ee3e
melody <melodylynnnh@gmail.com>
2022-12-01 13:13:49 -0500

    mock API

--- src/Tests/Recipe.test.tsx
+++ src/Tests/Recipe.test.tsx
@@ -2,18 +2,12 @@ import {render, screen, fireEvent} from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../App';
 import RecipePage from '../Pages/Recipe/RecipePage';
-import API from '../API';
-import TestAPI from "./TestAPI";
 
-jest.mock("../API", function() {
-    const testAPI = jest.requireActual('./TestAPI');
-    return testAPI;
-});
+jest.mock('../API');
 
 describe('recipe buttons',()=>{
-    test.skip('are clickable',()=>{
-        render(<Router><App/></Router>);
-        fireEvent.click(screen.getByText(/Recipes/));
+    test('are clickable',()=>{
+        render(<Router><RecipePage/></Router>);
         fireEvent.click(screen.getByText('Banana BreadThumbnail'));
         expect(screen.getByText(/20 years$/)).toBeInTheDocument();
     });
