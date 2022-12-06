ccf94dd105591481094836ad499e4aff54708ec0
melody <melodylynnnh@gmail.com>
2022-12-06 14:40:25 -0500

    cleanup

--- src/Pages/Calendar/Calendar.tsx
+++ src/Pages/Calendar/Calendar.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import {Calendar as ReactCalendar} from "react-calendar";
 import API from '../../API';
 import { FamilyMember } from '../../Structs/DataTypes';
-
+import familyMemberLayout from '../../Components/familyMemberLayout';
 
 const Calendar = () => {
     const [selectedDate, selectDay] = useState(new Date());
@@ -12,29 +12,7 @@ const Calendar = () => {
     useEffect(() => {
         API.getFamilyMembers().then((members) => setFamilyMembers(members));
     }, []);
-    const familyMembersLayout = () => {
-        let familyMemberNames: string[] = [];
-        let totalDurations: string[] = [];
-        members.forEach(function (member) {
-            member.exerciseLog.forEach(function (log) {
-                if (new Date(log.created_at).toLocaleDateString() === selectedDate.toLocaleDateString()) {
-                    if (familyMemberNames.includes(log.name)) {
-                        totalDurations[familyMemberNames.indexOf(log.name)] = `${parseInt(totalDurations[familyMemberNames.indexOf(log.name)]) + parseInt(log.duration)}`;
-                    } else {
-                        familyMemberNames.push(log.name);
-                        totalDurations[familyMemberNames.indexOf(log.name)] = log.duration;
-                    }
-                }
-            });
-    });
-        return familyMemberNames.map((member)=>{
-            return (
-                <div className='logs'>
-                    <p>{member + " logged "+ totalDurations[familyMemberNames.indexOf(member)] +" minutes of activity! " + (parseInt(totalDurations[familyMemberNames.indexOf(member)]) > 59 ? "Great job!" : "There's still time to hit 60 minutes today!")}</p>
-                </div>
-            )
-        });
-    }
+    
     return (
         <div className="calendar">
             <HelmetProvider>
@@ -46,7 +24,7 @@ const Calendar = () => {
             <br/>
             <h1>On {selectedDate.toLocaleDateString()}…</h1>
             <div className='logText'>
-                {familyMembersLayout().length > 0 ? familyMembersLayout() : "No one logged any activity on this day, but there's still time to log some for today!"}
+                {familyMemberLayout(members, selectedDate).length > 0 ? familyMemberLayout(members, selectedDate) : "No one logged any activity on this day, but there's still time to log some for today!"}
             </div>
         </div>
     )
