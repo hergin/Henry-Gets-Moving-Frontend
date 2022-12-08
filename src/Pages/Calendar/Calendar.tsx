78ab09e2adf7da9d9b94c3ab089b7aed75651fe9
Mars <ngwilliams@bsu.edu>
2022-12-07 19:33:19 -0500

    Add check for if there are no children

--- src/Pages/Calendar/Calendar.tsx
+++ src/Pages/Calendar/Calendar.tsx
@@ -23,80 +23,98 @@ const Calendar = () => {
         API.getFamilyMembers().then((members) => {
             setFamilyMembers(members)
             setFamilyMember(members[0])
+
         });
         API.getExerciseLogs().then((logs) => {
             setExerciseLogs(logs)
         })
     }, []);
-
-    return (
-        <div className="calendar">
-            <HelmetProvider>
-                <Helmet>
-                    <title>Calendar</title>
-                </Helmet>
-            </HelmetProvider>
-            <Weather/>
-            <BackArrow route={'/get-moving'}/>
-            <ReactCalendar onClickDay={(date) => {
-                selectDay(date)
-            }} value={selectedDate} minDate={new Date(2022, 9, 20)}
-                           className="date-picker"/>
-            <div className={"date-member-select"}>
-                <h1>On {selectedDate.toLocaleDateString()}…</h1>
-                <select onChange={(event) => {
-                    event.preventDefault()
-                    const index: number = parseInt(event.currentTarget.value, 10)
-                    setFamilyMember(familyMember => {
-                        return {...(members[index] as FamilyMember)}
-                    })
-                }}>
-                    {members && members.map((member, index) => {
+    if(!familyMember){
+        return (
+            <div className={"calendar"}>
+                <HelmetProvider>
+                    <Helmet>
+                        <title>Calendar</title>
+                    </Helmet>
+                </HelmetProvider>
+                <Weather/>
+                <BackArrow route={'/get-moving'}/>
+                <h4>You have never submitted an exercise log before! To view the calendar, get moving and submit your first log!</h4>
+                <Link className={'red-button'} to={'/exercise-log'}>Get Moving!</Link>
+            </div>
+        )
+    }
+    else{
+        return (
+            <div className="calendar">
+                <HelmetProvider>
+                    <Helmet>
+                        <title>Calendar</title>
+                    </Helmet>
+                </HelmetProvider>
+                <Weather/>
+                <BackArrow route={'/get-moving'}/>
+                <ReactCalendar onClickDay={(date) => {
+                    selectDay(date)
+                }} value={selectedDate} minDate={new Date(2022, 9, 20)}
+                               className="date-picker"/>
+                <div className={"date-member-select"}>
+                    <h1>On {selectedDate.toLocaleDateString()}…</h1>
+                    <select onChange={(event) => {
+                        event.preventDefault()
+                        const index: number = parseInt(event.currentTarget.value, 10)
+                        setFamilyMember(familyMember => {
+                            return {...(members[index] as FamilyMember)}
+                        })
+                    }}>
+                        {members && members.map((member, index) => {
+                            return (
+                                <option value={index}>{member.name}</option>
+                            )
+                        })}
+                    </select>
+                </div>
+                <div className='exercise-logs-div'>
+                    {(exerciseLogs && exerciseLogs.filter((log) => {return new Date(log.date).toDateString() == selectedDate.toDateString() && log.family_member_id == familyMember.id}).length != 0) &&
+                        <div className='legend'>
+                            <p>Name</p>
+                            <p>Exercise Type</p>
+                            <p>Duration</p>
+                            <p>Intensity</p>
+                        </div>}
+                    {(!exerciseLogs || exerciseLogs.filter((log) => {return new Date(log.date).toDateString() == selectedDate.toDateString() && log.family_member_id == familyMember.id}).length == 0) &&
+                        <div>
+                            <h5>There are no exercise logs for {familyMember.name} on this day.</h5>
+                            <Link className={'red-button'} to={'/exercise-log'}>Get Moving!</Link>
+                        </div>}
+                    {exerciseLogs && exerciseLogs.filter((log) => {
+                        return new Date(log.date).toDateString() == selectedDate.toDateString() && log.family_member_id == familyMember.id
+                    }).map((log) => {
                         return (
-                            <option value={index}>{member.name}</option>
+                            <div className='log'>
+                                <p>{log.name}</p>
+                                <p>{log.type}</p>
+                                <p>{log.duration} minutes</p>
+                                {log.intensity === "Light" &&
+                                    <img src={LightIntensity}/>
+                                }
+                                {log.intensity === "Moderate" &&
+                                    <img src={ModerateIntensity}/>
+                                }
+                                {log.intensity === "Vigorous" &&
+                                    <img src={VigorousIntensity}/>
+                                }
+                            </div>
                         )
                     })}
-                </select>
+                </div>
+                <div className='swings'>
+                    <img src={swings} alt={"Henry and Jasmine on Swings"}/>
+                </div>
+                <Grass/>
             </div>
-            <div className='exercise-logs-div'>
-                {(exerciseLogs && exerciseLogs.filter((log) => {return new Date(log.date).toDateString() == selectedDate.toDateString() && log.family_member_id == familyMember.id}).length != 0) &&
-                    <div className='legend'>
-                        <p>Name</p>
-                        <p>Exercise Type</p>
-                        <p>Duration</p>
-                        <p>Intensity</p>
-                    </div>}
-                {(!exerciseLogs || exerciseLogs.filter((log) => {return new Date(log.date).toDateString() == selectedDate.toDateString() && log.family_member_id == familyMember.id}).length == 0) &&
-                    <div>
-                        <h5>There are no exercise logs for {familyMember.name} on this day.</h5>
-                        <Link className={'red-button'} to={'/exercise-log'}>Get Moving!</Link>
-                    </div>}
-                {exerciseLogs && exerciseLogs.filter((log) => {
-                    return new Date(log.date).toDateString() == selectedDate.toDateString() && log.family_member_id == familyMember.id
-                }).map((log) => {
-                    return (
-                        <div className='log'>
-                            <p>{log.name}</p>
-                            <p>{log.type}</p>
-                            <p>{log.duration} minutes</p>
-                            {log.intensity === "Light" &&
-                            <img src={LightIntensity}/>
-                            }
-                            {log.intensity === "Moderate" &&
-                            <img src={ModerateIntensity}/>
-                            }
-                            {log.intensity === "Vigorous" &&
-                            <img src={VigorousIntensity}/>
-                            }
-                        </div>
-                    )
-                })}
-            </div>
-            <div className='swings'>
-                <img src={swings} alt={"Henry and Jasmine on Swings"}/>
-            </div>
-            <Grass/>
-        </div>
-    )
+        )
+    }
+
 }
 export default Calendar;
