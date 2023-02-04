ac4347bf9d129f619eca30871495e55687b6e81f
melody <melodylynnnh@gmail.com>
2023-02-03 19:25:16 -0500

    update game tests

--- src/Tests/Game.test.tsx
+++ src/Tests/Game.test.tsx
@@ -1,15 +1,11 @@
 import {render,screen} from '@testing-library/react';
 import Game from '../Pages/Game/Game';
 
-test.skip('maze exists',()=>{
-    render(<Game/>);
-    expect(screen.getByAltText('Squirm\'s maze')).toBeInTheDocument();
-});
-test.skip('crossword exists',()=>{
-    render(<Game/>);
-    expect(screen.getByAltText('Healthy Eating Crossword Thumbnail')).toBeInTheDocument();
-});
-test.skip('race exists',()=>{
-    render(<Game/>);
-    expect(screen.getByAltText('Henry\'s Big Race Thumbnail')).toBeInTheDocument();
+describe("crossword",()=>{
+    describe("fill all answers",()=>{
+        test('exists',()=>{
+            render(<Game/>);
+            expect(screen.getByText("Fill all answers")).toBeInTheDocument();
+        });
+    });
 });
