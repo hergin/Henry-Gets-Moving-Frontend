5169752f11adc5852e5448fda8ee62d22d430525
melody <melodylynnnh@gmail.com>
2023-02-03 21:08:32 -0500

    add tests cells

--- src/Tests/Game.test.tsx
+++ src/Tests/Game.test.tsx
@@ -1,4 +1,5 @@
 import {fireEvent, render,screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Game from '../Pages/Game/Game';
 
 describe("crossword",()=>{
@@ -29,6 +30,20 @@ describe("crossword",()=>{
             fireEvent.click(screen.getByText('Reset'));
             // and assert it's no longer filled
             expect(cell.innerHTML).toEqual('');
+        });
+    });
+    describe('cells',()=>{
+        test('can be typed in',()=>{
+            const {container} = render(<Game/>);
+            const cell = container.getElementsByClassName('guess-text-incorrect')[0];
+            userEvent.type(cell, "B");
+            expect(cell.innerHTML).toEqual('B');
+        });
+        test('turn yellow when correct',()=>{
+            const {container} = render(<Game/>);
+            const random_cell = container.getElementsByClassName('guess-text-incorrect')[0];
+            fireEvent.click(screen.getByText('Fill all answers'));
+            expect(random_cell).toHaveClass('guess-text-correct');
         });
     });
 });
