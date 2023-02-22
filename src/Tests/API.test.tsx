1e1f7d965184955c49544a7db3ea16830783467b
melody <melodylynnnh@gmail.com>
2023-02-22 12:34:25 -0500

    add test embed link

--- src/Tests/API.test.tsx
+++ src/Tests/API.test.tsx
@@ -7,4 +7,7 @@ describe("parse embed",()=>{
     test("playlist link",()=>{
         expect(API.parseEmbedLink("https://www.youtube.com/watch?v=1wGOHbcQKIc&list=PL0nhBlgFkftzaBqgQjvo-1z81P1vkBJYN&index=1")).toEqual("https://www.youtube.com/embed/1wGOHbcQKIc");
     });
+    test("embed link",()=>{
+        expect(API.parseEmbedLink("https://www.youtube.com/embed/1wGOHbcQKIc")).toEqual("https://www.youtube.com/embed/1wGOHbcQKIc");
+    });
 });
