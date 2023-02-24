4be457e74011d2d40fd65dc118d8d6fd75910c9a
melody <melodylynnnh@gmail.com>
2023-02-24 13:53:27 -0500

    despise promises

--- src/Pages/Calendar/Calendar.tsx
+++ src/Pages/Calendar/Calendar.tsx
@@ -64,13 +64,14 @@ const Calendar = () => {
                     selectDay(date)
                 }} value={selectedDate} minDate={new Date(2022, 9, 20)}
                                className="date-picker"
-                               tileClassName={({date}) => {
-                                    let time = 0;
-                                    API.getTotalLoggedDuration(familyMember.name, date).then((duration)=>time=duration);
-                                    if (time > 0 && time < 60) return "any-minutes-logged";
-                                    else if (time > 59) return "sixty-minutes-logged";
-                                    else return null;
-                               }}/>
+                               tileClassName={({date, view}) => {
+                                if (view === 'month'){
+                                    return API.getTotalLoggedDuration(familyMember.name, date).then((duration)=>{
+                                        if (duration > 0 && duration < 60) return "any-minutes-logged";
+                                        else if (duration > 59) return "sixty-minutes-logged";
+                                        else return 'no-minutes-logged';
+                                    });
+                               }else return 'no-minutes-logged';}}/>
                 <div className={"date-member-select"}>
                     <h1>On {selectedDate.toLocaleDateString()}…</h1>
                     <select onChange={(event) => {
