91c3b5e509a3bcc31040ce240c8887a9d739f015
Kiri Woodruff <kdwoodruff@bsu.edu>
2023-04-11 20:44:24 -0400

    Add edit button

--- src/Pages/Calendar/Calendar.tsx
+++ src/Pages/Calendar/Calendar.tsx
@@ -12,6 +12,7 @@ import LightIntensity from '../../Assets/LightIntensity.svg'
 import ModerateIntensity from '../../Assets/ModerateIntensity.svg'
 import VigorousIntensity from '../../Assets/VigorousIntensity.svg'
 import {Link} from "react-router-dom";
+import Edit from '../../Assets/Edit.svg'
 
 const Calendar = () => {
     async function deleteLog(logToDelete: ExerciseLog) {
@@ -34,6 +35,7 @@ const Calendar = () => {
     const [familyMember, setFamilyMember] = useState({} as FamilyMember)
     const [exerciseLogs, setExerciseLogs] = useState([] as ExerciseLog[])
     const [dailyTime, setDailyTime] = useState(0)
+    const [selectedLog, setSelectedLog] = useState<null | ExerciseLog>(null);
     useEffect(() => {
         API.getFamilyMembers().then((members) => {
             setFamilyMembers(members)
@@ -102,7 +104,7 @@ const Calendar = () => {
                             <p>Exercise Type</p>
                             <p>Duration</p>
                             <p>Intensity</p>
-                            <p>Delete</p>
+                            <p>Edit</p>
                         </div>}
                     {(!exerciseLogs || exerciseLogs.filter((log) => {return new Date(log.date).toDateString() == selectedDate.toDateString() && log.family_member_id == familyMember.id}).length == 0) &&
                         <div className='no-logs'>
@@ -126,7 +128,7 @@ const Calendar = () => {
                                 {log.intensity === "Vigorous" &&
                                     <img src={VigorousIntensity}/>
                                 }
-                                {<button className={'red-button'} onClick={function () {deleteLog(log)}}>Delete</button>}
+                                {<button className={'edit-button'} onClick={e => (setSelectedLog(log))}><img src={Edit} alt={"Edit"}/></button>}
                             </div>
                         )
                     })}
